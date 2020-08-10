6680be7de30e63b50112145949e10b4af030f0ae
David Coats <david@getsafetycone.com>
2020-08-10 12:19:43 -0700

    Added RepositoryDirectoryPath type.

---
+++ source/R5T.T0010/Code/Classes/RepositoryDirectoryPath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.T0010
+{
+    public class RepositoryDirectoryPath : TypedString
+    {
+        #region Static
+
+        public static RepositoryDirectoryPath From(string repositoryDirectoryPathValue)
+        {
+            var repositoryDirectoryPath = new RepositoryDirectoryPath(repositoryDirectoryPathValue);
+            return repositoryDirectoryPath;
+        }
+
+        #endregion
+
+
+        public RepositoryDirectoryPath(string value)
+            : base(value)
+        {
+        }
+    }
+}
