d8716dc5c2c835e9e468791adc19e0ea41efa0ee
David Coats <david@getsafetycone.com>
2020-08-29 17:09:09 -0700

    Add RepositoryNameAndRemoteUrlMapping and -List types

---
+++ source/R5T.T0010/Code/Classes/RepositoryNameAndRemoteUrlMapping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace R5T.T0010
+{
+    public class RepositoryNameAndRemoteUrlMapping
+    {
+        public RepositoryName RepositoryName { get; set; }
+        public RemoteRepositoryUrl RemoteRepositoryUrl { get; set; }
+
+
+        public RepositoryNameAndRemoteUrlMapping()
+        {
+        }
+
+        public RepositoryNameAndRemoteUrlMapping(
+            RepositoryName repositoryName,
+            RemoteRepositoryUrl remoteRepositoryUrl)
+        {
+            this.RepositoryName = repositoryName;
+            this.RemoteRepositoryUrl = remoteRepositoryUrl;
+        }
+    }
+}
---
+++ source/R5T.T0010/Code/Classes/RepositoryNameAndRemoteUrlMappingsList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Magyar;
+
+
+namespace R5T.T0010
+{
+    public class RepositoryNameAndRemoteUrlMappingsList : TypedList<RepositoryNameAndRemoteUrlMapping>
+    {
+        public RepositoryNameAndRemoteUrlMappingsList()
+        {
+        }
+
+        public RepositoryNameAndRemoteUrlMappingsList(IEnumerable<RepositoryNameAndRemoteUrlMapping> values)
+            : base(values)
+        {
+        }
+    }
+}
